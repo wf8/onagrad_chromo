85e0bb9191c09b970339fa0edae7afc79fec296a
wf <willfreyman@gmail.com>
2016-03-17 13:43:29 -0700

    final seq chromo runs

--- analyses/sequential/chromosomes/build_ancestral_state_trace.py
+++ analyses/sequential/chromosomes/build_ancestral_state_trace.py
@@ -5,10 +5,11 @@ import itertools
 import operator
 
 
-f = "output/chromo-anc1.log"
+f = ["output/chromo-anc1.log", "output/chromo-anc2.log"]
 n = 340 # number of tips
 burnin = 100
 
+# node ids
 root = 678
 root_state = []
 base_lyth = 677
@@ -17,22 +18,30 @@ base_onag = 630
 base_onag_states = []
 
 final_csv = []
-with open(f, 'rb') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter="\t")
-    for j, row in enumerate(csvreader):
-        final_row = []
-        for i, column in enumerate(row):
-            if i == 0 or (i > 2 * n and i % 2 == 0):
-                final_row.append(column)
-                if j + 1 > burnin:
-                    current_index = (i / 2) - 1
-                    if current_index == root:
-                        root_state.append(column)
-                    if current_index == base_lyth:
-                        base_lyth_states.append(column)
-                    if current_index == base_onag:
-                        base_onag_states.append(column)
-        final_csv.append(final_row)
+gen = 0
+header = False
+for log in f:
+    with open(log, 'rb') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter="\t")
+        for j, row in enumerate(csvreader):
+            if gen == 0 or j > 1:
+                final_row = []
+                for i, column in enumerate(row):
+                    if i == 0 and header == True:
+                        final_row.append(gen)
+                        gen += 1
+                    elif i == 0 or (i > 2 * n and i % 2 == 0):
+                        final_row.append(column)
+                        if j + 1 > burnin:
+                            current_index = (i / 2) - 1
+                            if current_index == root:
+                                root_state.append(column)
+                            if current_index == base_lyth:
+                                base_lyth_states.append(column)
+                            if current_index == base_onag:
+                                base_onag_states.append(column)
+                header = True
+                final_csv.append(final_row)
 
 root_map = max(set(root_state), key=root_state.count)
 lyth_map = max(set(base_lyth_states), key=base_lyth_states.count)
@@ -46,7 +55,7 @@ print("Root MAP = " + str(root_map) + ", pp = " + str(root_pp))
 print("Base Lyth MAP = " + str(lyth_map) + ", pp = " + str(lyth_pp))
 print("Base Onag MAP = " + str(onag_map) + ", pp = " + str(onag_pp))
 
-with open(f + ".final.log", "wb") as csvfile:
+with open("output/chromo-anc-final.log", "wb") as csvfile:
     csvwriter = csv.writer(csvfile, delimiter="\t")
     for row in final_csv:
         csvwriter.writerow(row)
--- analyses/sequential/chromosomes/check_rjmcmc.py
+++ analyses/sequential/chromosomes/check_rjmcmc.py
@@ -5,7 +5,7 @@ import itertools
 import operator
 
 
-f = "output/chromo1.log"
+files = ["output/chromo1.log", "output/chromo2.log"]
 
 
 #Iteration   Posterior   Likelihood  Prior   clado_demipoly  clado_fission   clado_fusion    clado_polyploid clado_type[1]   clado_type[2]   clado_type[3]   clado_type[4]   delta   delta_l lambda  lambda_l    mu  rho
@@ -48,82 +48,83 @@ def most_common(L):
 
 bitstrings = []
 
-with open(f, 'rb') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter="\t")
-    for row in csvreader:
-        if row[0] != "Iteration":
+for f in files:
+    with open(f, 'rb') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter="\t")
+        for row in csvreader:
+            if row[0] != "Iteration":
 
-            bitstring = ""
-            samples += 1
+                bitstring = ""
+                samples += 1
 
-            if row[4] == "0":
-                clado_demipoly += 1
-                bitstring += "0"
-            else:
-                bitstring += "1"
-            if row[5] == "0":
-                clado_fission += 1
-                bitstring += "0"
-            else:
-                bitstring += "1"
-            if row[6] == "0":
-                clado_fusion += 1
-                bitstring += "0"
-            else:
-                bitstring += "1"
-            if row[7] == "0":
-                clado_polyploid += 1
-                bitstring += "0"
-            else:
-                bitstring += "1"
-            
-            if row[4] == "0" and row[5] == "0" and row[6] == "0" and row[7] == "0":
-                all_ana += 1
+                if row[4] == "0":
+                    clado_demipoly += 1
+                    bitstring += "0"
+                else:
+                    bitstring += "1"
+                if row[5] == "0":
+                    clado_fission += 1
+                    bitstring += "0"
+                else:
+                    bitstring += "1"
+                if row[6] == "0":
+                    clado_fusion += 1
+                    bitstring += "0"
+                else:
+                    bitstring += "1"
+                if row[7] == "0":
+                    clado_polyploid += 1
+                    bitstring += "0"
+                else:
+                    bitstring += "1"
+                
+                if row[4] == "0" and row[5] == "0" and row[6] == "0" and row[7] == "0":
+                    all_ana += 1
 
-            if row[12] == "0":
-                delta += 1
-                bitstring += "0"
-            else:
-                bitstring += "1"
-            if row[13] == "0" or (row[13] != "0" and row[12] == "0"):
-                delta_l += 1
-                bitstring += "0"
-            else:
-                bitstring += "1"
-            if row[14] == "0":
-                lambda_ += 1
-                bitstring += "0"
-            else:
-                bitstring += "1"
-            if row[15] == "0" or (row[15] != "0" and row[14] == "0"):
-                lambda_l += 1
-                bitstring += "0"
-            else:
-                bitstring += "1"
-            if row[16] == "0":
-                mu += 1
-                bitstring += "0"
-            else:
-                bitstring += "1"
-            if row[17] == "0":
-                rho += 1
-                bitstring += "0"
-            else:
-                bitstring += "1"
+                if row[12] == "0":
+                    delta += 1
+                    bitstring += "0"
+                else:
+                    bitstring += "1"
+                if row[13] == "0" or (row[13] != "0" and row[12] == "0"):
+                    delta_l += 1
+                    bitstring += "0"
+                else:
+                    bitstring += "1"
+                if row[14] == "0":
+                    lambda_ += 1
+                    bitstring += "0"
+                else:
+                    bitstring += "1"
+                if row[15] == "0" or (row[15] != "0" and row[14] == "0"):
+                    lambda_l += 1
+                    bitstring += "0"
+                else:
+                    bitstring += "1"
+                if row[16] == "0":
+                    mu += 1
+                    bitstring += "0"
+                else:
+                    bitstring += "1"
+                if row[17] == "0":
+                    rho += 1
+                    bitstring += "0"
+                else:
+                    bitstring += "1"
 
-            bitstrings.append(bitstring)
+                bitstrings.append(bitstring)
 
-            if row[12] == "0" and row[14] == "0" and row[16] == "0" and row[17] == "0":
-                all_clado += 1
+                if row[12] == "0" and row[14] == "0" and row[16] == "0" and row[17] == "0":
+                    all_clado += 1
 
-            if row[7] != "0" and row[17] == "0":
-                clado_poly_no_ana_poly += 1
-            if row[7] == "0" and row[17] != "0":
-                ana_poly_no_clado_poly += 1
-            if row[7] != "0" and row[17] != "0":
-                both_poly += 1
-            if row[7] != "0" or row[17] != "0":
-                either_poly += 1
+                if row[7] != "0" and row[17] == "0":
+                    clado_poly_no_ana_poly += 1
+                if row[7] == "0" and row[17] != "0":
+                    ana_poly_no_clado_poly += 1
+                if row[7] != "0" and row[17] != "0":
+                    both_poly += 1
+                if row[7] != "0" or row[17] != "0":
+                    either_poly += 1
 
 maxx = most_common(bitstrings)
 print("MAP model = " + maxx)
