91a7ef13721de91cb21ad1e6ba452d6d8ef87e17
wf <willfreyman@gmail.com>
2016-07-14 17:01:22 -0700

    best model results

--- analyses/sequential/chromosomes/build_ancestral_state_trace.py
+++ analyses/sequential/chromosomes/build_ancestral_state_trace.py
@@ -9,8 +9,13 @@ f = ["output/chromo-anc1.log", "output/chromo-anc2.log",
      "output/chromo-anc3.log", "output/chromo-anc4.log",
      "output/chromo-anc5.log"]
 
+t = ["output/chromo1.trees", "output/chromo2.trees",
+     "output/chromo3.trees", "output/chromo4.trees",
+     "output/chromo5.trees"]
+
 n = 340 # number of tips
-burnin = 20 # per trace
+burnin = 100 # per trace
+sample_freq = 2 # number of iterations per sample
 
 # node ids
 root = 678
@@ -20,30 +25,33 @@ base_lyth_states = []
 base_onag = 630
 base_onag_states = []
 
+print("Combining ancestral state traces...")
+
 final_csv = []
 gen = 0
-header = False
+header_done = False
 for log in f:
     with open(log, 'rb') as csvfile:
         csvreader = csv.reader(csvfile, delimiter="\t")
         for j, row in enumerate(csvreader):
-            if gen == 0 or j > 1:
+            if not header_done:
+                final_csv.append(row)
+                header_done = True
+            elif j + 1 > burnin:
                 final_row = []
                 for i, column in enumerate(row):
-                    if i == 0 and header == True:
+                    if i == 0:
                         final_row.append(gen)
-                        gen += 1
-                    elif i == 0 or (i > 2 * n and i % 2 == 0):
+                        gen += sample_freq
+                    else:
                         final_row.append(column)
-                        if j + 1 > burnin:
-                            current_index = (i / 2) - 1
-                            if current_index == root:
-                                root_state.append(column)
-                            if current_index == base_lyth:
-                                base_lyth_states.append(column)
-                            if current_index == base_onag:
-                                base_onag_states.append(column)
-                header = True
+                        current_index = (i / 2) - 1
+                        if current_index == root:
+                            root_state.append(column)
+                        if current_index == base_lyth:
+                            base_lyth_states.append(column)
+                        if current_index == base_onag:
+                            base_onag_states.append(column)
                 final_csv.append(final_row)
 
 root_map = max(set(root_state), key=root_state.count)
@@ -62,3 +70,32 @@ with open("output/chromo-anc-final.log", "wb") as csvfile:
     csvwriter = csv.writer(csvfile, delimiter="\t")
     for row in final_csv:
         csvwriter.writerow(row)
+
+print("Combining tree traces...")
+
+final_csv = []
+gen = 0
+header_done = False
+for log in t:
+    with open(log, 'rb') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter="\t")
+        for j, row in enumerate(csvreader):
+            if not header_done:
+                final_csv.append(row)
+                header_done = True
+            elif j + 1 > burnin:
+                final_row = []
+                for i, column in enumerate(row):
+                    if i == 0:
+                        final_row.append(gen)
+                        gen += sample_freq
+                    else:
+                        final_row.append(column)
+                final_csv.append(final_row)
+
+with open("output/chromo-anc-final.trees", "wb") as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter="\t")
+    for row in final_csv:
+        csvwriter.writerow(row)
+
+print("Done.")
