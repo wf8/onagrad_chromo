70d5e97276d21adb4e60ffa7447ff02dba62658b
RevBayes <revbayes@bayes3.biol.berkeley.edu>
2016-12-27 16:43:33 -0800

    using different seed values

--- analyses/sequential/chromosse/run.sh
+++ analyses/sequential/chromosse/run.sh
@@ -3,9 +3,10 @@
 rm -rf temp_output
 mkdir temp_output
 
-for rep in {1..2}
+for rep in {1..24}
 do
-    echo "rep = $rep" > ${rep}.Rev
+    echo "rep = ${rep}" > ${rep}.Rev
+    echo "seed(${rep})" >> ${rep}.Rev
     cat onagrad.Rev >> ${rep}.Rev
     rb ${rep}.Rev > temp_output/${rep}.out 2>&1 &
     sleep 0.5
